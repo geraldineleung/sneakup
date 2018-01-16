bab984578c297246c97e21994fea7a0e688d9d5e
Geraldine Leung <geraldine.leung2@gmail.com>
2018-01-16 15:10:19 -0500

    Created loadlevel and unloadlevel functions Added spawn positions for each additional human at different game levels

--- js/index.js
+++ js/index.js
@@ -3,10 +3,12 @@ var upBody, downBody, leftBody, rightBody;
 var hitboxArr = [], humanArr = [];
 var ghostX, ghostY;
 var ghostText, humanText, scoreText, levelText;
-var score = 0, level=1;
+var score = 0, currentLevel=0;
 
 function preload(){
-  game.load.image('room', 'assets/room.png');
+  for (var i = 0; i < levels.length; i++) {
+    game.load.image('level'+i, levels[i].background);
+  }
   game.load.spritesheet('boy', 'assets/george.png', 48, 48);
   game.load.spritesheet('girl', 'assets/betty.png', 48, 48);
   game.load.spritesheet('ghost', 'assets/boo.png', 52, 40);
@@ -14,22 +16,14 @@ function preload(){
 
 function create(){
   game.physics.startSystem(Phaser.Physics.P2JS);
-  game.add.sprite(0,0, 'room');
+  loadLevel(currentLevel);
 
-  for(var i=0; i<level; i++){
-      humanArr[i] = new human('boy', game.world.randomX, game.world.randomY);
-  }
-
-  // boy2 = new human('boy', game.world.centerX - 50, game.world.centerY - 250);
-  // girl2 = new human('girl', game.world.centerX + 50, game.world.centerY - 100);
-  // humanArr.push(boy1, boy2, girl1, girl2);
-
-  ghostX = 327;
+  ghostX = 350;
   ghostY = 30;
 
   ghost = game.add.sprite(ghostX, ghostY, 'ghost');
   ghost.scale.setTo(0.6,0.6);
-  game.physics.p2.enable(ghost, true);//true means the hitbox is visible
+  game.physics.p2.enable(ghost, false);//true means the hitbox is visible
   ghost.body.immovable = true;
   ghost.animations.add('left', [2], 10, true);
   ghost.animations.add('right', [3], 10, true);
@@ -37,67 +31,14 @@ function create(){
   ghost.animations.add('down', [0], 10, true);
   ghost.body.fixedRotation = true;
 
-  hitbox1 = game.add.graphics(100,470);
-  hitbox1.drawRect(0,0,200,210);
-  hitboxArr.push(hitbox1);
-
-  hitbox2 = game.add.graphics(100,65);
-  hitbox2.drawRect(0,0,200,130);
-  hitboxArr.push(hitbox2);
-
-  hitbox3 = game.add.graphics(585,65);
-  hitbox3.drawRect(0,0,200,130);
-  hitboxArr.push(hitbox3);
-
-  hitbox4 = game.add.graphics(585,455);
-  hitbox4.drawRect(0,0,200,180);
-  hitboxArr.push(hitbox4);
-
-  hitbox5 = game.add.graphics(342,475);
-  hitbox5.drawRect(0,0,160,95);
-  hitboxArr.push(hitbox5);
-
-  hitbox6 = game.add.graphics(183,205);
-  hitbox6.drawRect(0,0,20,50);
-  hitboxArr.push(hitbox6);
-
-  hitbox7 = game.add.graphics(503,205);
-  hitbox7.drawRect(0,0,20,50);
-  hitboxArr.push(hitbox7);
-
-  hitbox8 = game.add.graphics(503,300);
-  hitbox8.drawRect(0,0,20,50);
-  hitboxArr.push(hitbox8);
-
-  hitbox9 = game.add.graphics(183,300);
-  hitbox9.drawRect(0,0,20,50);
-  hitboxArr.push(hitbox9);
-
-  hitbox10 = game.add.graphics(630,560);
-  hitbox10.drawRect(0,0,35,35);
-  hitboxArr.push(hitbox10);
-
-  hitbox11 = game.add.graphics(595,180);
-  hitbox11.drawRect(0,0,60,75);
-  hitboxArr.push(hitbox11);
-
-  hitbox12 = game.add.graphics(600,230);
-  hitbox12.drawRect(0,0,20,20);
-  hitboxArr.push(hitbox12);
-
-  for(var i=0; i<hitboxArr.length; i++){
-    game.physics.p2.enable(hitboxArr[i], false);
-    hitboxArr[i].body.static = true;
-  }
   ghostText = this.add.text(game.world.centerX, game.world.centerY, "", {size: "12px", fill: (0,0,0), align:"center"});
   humanText = this.add.text(game.world.centerX-100, game.world.centerY-100, "", {size: "12px", fill: (0,0,0), align:"center"});
   scoreText = this.add.text(10, 10, "", {size: "12px", fill: (0,0,0), align:"center"});
   levelText = this.add.text(10, 40, "", {size: "12px", fill: (0,0,0), align:"center"});
   scoreText.setText("Score: " + score);
-  levelText.setText("Level: " + level);
+  levelText.setText("Level: " + currentLevel);
   //losing condition: when ghost is in contact with triangle body shape of human
   ghost.body.onBeginContact.add(ghostCollision, this);
-
 }
 
 function ghostCollision(body, bodyB, shapeA, shapeB, equation){
@@ -116,30 +57,26 @@ function ghostCollision(body, bodyB, shapeA, shapeB, equation){
       humanArr[humanIndex].sprite.kill();
       humanArr.splice(humanIndex, 1);
       console.log(humanArr);
-
-      if(humanArr.length == 0){
-        level += 1;
-      }
     }
-    else if(shapeB.constructor.name == 'Convex'){
-      // humanText.setText("Ahhhhh!");
+    else if(shapeB.constructor.name == 'Convex'){//human sees ghost
 
       if(humanArr[humanIndex].direction == 'up'){
         humanArr[humanIndex].direction = 'down';
-        humanArr[humanIndex].speed = 50;
+        humanArr[humanIndex].speed = 60;
       }
       else if(humanArr[humanIndex].direction == 'down'){
         humanArr[humanIndex].direction = 'up';
-        humanArr[humanIndex].speed = 50;
+        humanArr[humanIndex].speed = 60;
       }
       else if(humanArr[humanIndex].direction == 'right'){
         humanArr[humanIndex].direction = 'left';
-        humanArr[humanIndex].speed = 50;
+        humanArr[humanIndex].speed = 60;
       }
       else if(humanArr[humanIndex].direction == 'left'){
         humanArr[humanIndex].direction = 'right';
-        humanArr[humanIndex].speed = 50;
+        humanArr[humanIndex].speed = 60;
       }
+
     }
   }
 }
@@ -175,7 +112,15 @@ function update(){
       humanArr[i].update();
   }
 
-
+  if(humanArr.length == 0){
+    unloadLevel(currentLevel);
+    currentLevel +=1;
+    loadLevel(currentLevel);
+    game.world.bringToTop(ghost);
+    game.world.bringToTop(scoreText);
+    game.world.bringToTop(levelText);
+    // console.log(currentLevel);
+  }
 
 }
 
---
+++ js/levels.js
@@ -0,0 +1,164 @@
+var levels = [
+  {//1st level
+    background:"assets/room.png",
+    humans:1,
+    ghostPosition: {x:350, y:30},
+    hitboxes:[{x:100, y:470, width:200, height:210}, {x:100, y:65, width:200, height:130},{x:585, y:65, width:200, height:130},
+      {x:585, y:455, width:200, height:180},{x:342, y:475, width:160, height:95},{x:183, y:205, width:20, height:50},
+      {x:503, y:205, width:20, height:50},{x:503, y:300, width:20, height:50},{x:183, y:300, width:20, height:50},
+      {x:630, y:560, width:35, height:35},{x:595, y:180, width:60, height:75},{x:600, y:230, width:20, height:20}]
+  },
+  {//2nd level
+    background:"assets/room.png",
+    humans:2,
+    ghostPosition: {x:350, y:30},
+    hitboxes:[{x:100, y:470, width:200, height:210}, {x:100, y:65, width:200, height:130},{x:585, y:65, width:200, height:130},
+      {x:585, y:455, width:200, height:180},{x:342, y:475, width:160, height:95},{x:183, y:205, width:20, height:50},
+      {x:503, y:205, width:20, height:50},{x:503, y:300, width:20, height:50},{x:183, y:300, width:20, height:50},
+      {x:630, y:560, width:35, height:35},{x:595, y:180, width:60, height:75},{x:600, y:230, width:20, height:20}]
+  },
+  {//3rd level
+    background:"assets/room.png",
+    humans:3,
+    ghostPosition: {x:350, y:30},
+    hitboxes:[{x:100, y:470, width:200, height:210}, {x:100, y:65, width:200, height:130},{x:585, y:65, width:200, height:130},
+      {x:585, y:455, width:200, height:180},{x:342, y:475, width:160, height:95},{x:183, y:205, width:20, height:50},
+      {x:503, y:205, width:20, height:50},{x:503, y:300, width:20, height:50},{x:183, y:300, width:20, height:50},
+      {x:630, y:560, width:35, height:35},{x:595, y:180, width:60, height:75},{x:600, y:230, width:20, height:20}]
+  },
+  {//4th level
+    background:"assets/room.png",
+    humans:4,
+    hitboxes:[{x:100, y:470, width:200, height:210}, {x:100, y:65, width:200, height:130},{x:585, y:65, width:200, height:130},
+      {x:585, y:455, width:200, height:180},{x:342, y:475, width:160, height:95},{x:183, y:205, width:20, height:50},
+      {x:503, y:205, width:20, height:50},{x:503, y:300, width:20, height:50},{x:183, y:300, width:20, height:50},
+      {x:630, y:560, width:35, height:35},{x:595, y:180, width:60, height:75},{x:600, y:230, width:20, height:20}]
+  },
+  {//5th level
+    background:"assets/room.png",
+    humans:5,
+    hitboxes:[{x:100, y:470, width:200, height:210}, {x:100, y:65, width:200, height:130},{x:585, y:65, width:200, height:130},
+      {x:585, y:455, width:200, height:180},{x:342, y:475, width:160, height:95},{x:183, y:205, width:20, height:50},
+      {x:503, y:205, width:20, height:50},{x:503, y:300, width:20, height:50},{x:183, y:300, width:20, height:50},
+      {x:630, y:560, width:35, height:35},{x:595, y:180, width:60, height:75},{x:600, y:230, width:20, height:20}]
+  },
+  {//6th level
+    background:"assets/room.png",
+    humans:6,
+    hitboxes:[{x:100, y:470, width:200, height:210}, {x:100, y:65, width:200, height:130},{x:585, y:65, width:200, height:130},
+      {x:585, y:455, width:200, height:180},{x:342, y:475, width:160, height:95},{x:183, y:205, width:20, height:50},
+      {x:503, y:205, width:20, height:50},{x:503, y:300, width:20, height:50},{x:183, y:300, width:20, height:50},
+      {x:630, y:560, width:35, height:35},{x:595, y:180, width:60, height:75},{x:600, y:230, width:20, height:20}]
+  },
+  {//7th level
+    background:"assets/room.png",
+    humans:7,
+    hitboxes:[{x:100, y:470, width:200, height:210}, {x:100, y:65, width:200, height:130},{x:585, y:65, width:200, height:130},
+      {x:585, y:455, width:200, height:180},{x:342, y:475, width:160, height:95},{x:183, y:205, width:20, height:50},
+      {x:503, y:205, width:20, height:50},{x:503, y:300, width:20, height:50},{x:183, y:300, width:20, height:50},
+      {x:630, y:560, width:35, height:35},{x:595, y:180, width:60, height:75},{x:600, y:230, width:20, height:20}]
+  },
+  {//8th level
+    background:"assets/room.png",
+    humans:8,
+    hitboxes:[{x:100, y:470, width:200, height:210}, {x:100, y:65, width:200, height:130},{x:585, y:65, width:200, height:130},
+      {x:585, y:455, width:200, height:180},{x:342, y:475, width:160, height:95},{x:183, y:205, width:20, height:50},
+      {x:503, y:205, width:20, height:50},{x:503, y:300, width:20, height:50},{x:183, y:300, width:20, height:50},
+      {x:630, y:560, width:35, height:35},{x:595, y:180, width:60, height:75},{x:600, y:230, width:20, height:20}]
+  },
+  {//9th level
+    background:"assets/room.png",
+    humans:9,
+    hitboxes:[{x:100, y:470, width:200, height:210}, {x:100, y:65, width:200, height:130},{x:585, y:65, width:200, height:130},
+      {x:585, y:455, width:200, height:180},{x:342, y:475, width:160, height:95},{x:183, y:205, width:20, height:50},
+      {x:503, y:205, width:20, height:50},{x:503, y:300, width:20, height:50},{x:183, y:300, width:20, height:50},
+      {x:630, y:560, width:35, height:35},{x:595, y:180, width:60, height:75},{x:600, y:230, width:20, height:20}]
+  },
+  {//10th level
+    background:"assets/room.png",
+    humans:10,
+    hitboxes:[{x:100, y:470, width:200, height:210}, {x:100, y:65, width:200, height:130},{x:585, y:65, width:200, height:130},
+      {x:585, y:455, width:200, height:180},{x:342, y:475, width:160, height:95},{x:183, y:205, width:20, height:50},
+      {x:503, y:205, width:20, height:50},{x:503, y:300, width:20, height:50},{x:183, y:300, width:20, height:50},
+      {x:630, y:560, width:35, height:35},{x:595, y:180, width:60, height:75},{x:600, y:230, width:20, height:20}]
+  },
+  {//11th level
+    background:"assets/room.png",
+    humans:11,
+    hitboxes:[{x:100, y:470, width:200, height:210}, {x:100, y:65, width:200, height:130},{x:585, y:65, width:200, height:130},
+      {x:585, y:455, width:200, height:180},{x:342, y:475, width:160, height:95},{x:183, y:205, width:20, height:50},
+      {x:503, y:205, width:20, height:50},{x:503, y:300, width:20, height:50},{x:183, y:300, width:20, height:50},
+      {x:630, y:560, width:35, height:35},{x:595, y:180, width:60, height:75},{x:600, y:230, width:20, height:20}]
+  },
+  {//12th level
+    background:"assets/room.png",
+    humans:12,
+    hitboxes:[{x:100, y:470, width:200, height:210}, {x:100, y:65, width:200, height:130},{x:585, y:65, width:200, height:130},
+      {x:585, y:455, width:200, height:180},{x:342, y:475, width:160, height:95},{x:183, y:205, width:20, height:50},
+      {x:503, y:205, width:20, height:50},{x:503, y:300, width:20, height:50},{x:183, y:300, width:20, height:50},
+      {x:630, y:560, width:35, height:35},{x:595, y:180, width:60, height:75},{x:600, y:230, width:20, height:20}]
+  },
+  {//13th level
+    background:"assets/room.png",
+    humans:13,
+    hitboxes:[{x:100, y:470, width:200, height:210}, {x:100, y:65, width:200, height:130},{x:585, y:65, width:200, height:130},
+      {x:585, y:455, width:200, height:180},{x:342, y:475, width:160, height:95},{x:183, y:205, width:20, height:50},
+      {x:503, y:205, width:20, height:50},{x:503, y:300, width:20, height:50},{x:183, y:300, width:20, height:50},
+      {x:630, y:560, width:35, height:35},{x:595, y:180, width:60, height:75},{x:600, y:230, width:20, height:20}]
+  },
+  {//14th level
+    background:"assets/room.png",
+    humans:14,
+    hitboxes:[{x:100, y:470, width:200, height:210}, {x:100, y:65, width:200, height:130},{x:585, y:65, width:200, height:130},
+      {x:585, y:455, width:200, height:180},{x:342, y:475, width:160, height:95},{x:183, y:205, width:20, height:50},
+      {x:503, y:205, width:20, height:50},{x:503, y:300, width:20, height:50},{x:183, y:300, width:20, height:50},
+      {x:630, y:560, width:35, height:35},{x:595, y:180, width:60, height:75},{x:600, y:230, width:20, height:20}]
+  },
+  {//15th level
+    background:"assets/room.png",
+    humans:15,
+    hitboxes:[{x:100, y:470, width:200, height:210}, {x:100, y:65, width:200, height:130},{x:585, y:65, width:200, height:130},
+      {x:585, y:455, width:200, height:180},{x:342, y:475, width:160, height:95},{x:183, y:205, width:20, height:50},
+      {x:503, y:205, width:20, height:50},{x:503, y:300, width:20, height:50},{x:183, y:300, width:20, height:50},
+      {x:630, y:560, width:35, height:35},{x:595, y:180, width:60, height:75},{x:600, y:230, width:20, height:20}]
+  }
+];
+
+var initialX = 342;
+var initialY = 350;
+
+var positions = [{x:initialX, y: initialY}, {x:initialX-200, y: initialY+250}, {x:initialX+200, y: initialY-200}, {x:initialX-200, y: initialY-200}, {x:initialX-150, y: initialY+150},
+      {x:initialX+200, y: initialY+200}, {x:initialX+260, y: initialY+275}, {x:initialX, y: initialY+300}, {x:initialX+50, y: initialY+50}, {x:initialX+260, y: initialY},
+      {x:initialX+280, y: initialY-50}, {x:initialX+75, y: initialY+50}, {x:initialX-180, y: initialY+50}, {x:initialX, y: initialY-160}, {x:initialX-40, y: initialY+120}];
+// var ghost;
+function loadLevel(index){
+  var level = levels[index];
+  level.sprite = game.add.sprite(0,0, 'level'+index);
+  // ghost = game.add.sprite(levels[index].ghostPosition.x, levels[index].ghostPosition.y);
+  // ghost.body.velocity.x = 0;
+  // ghost.body.velocity.y = 0;
+  for (let i = 0; i < level.hitboxes.length; i++) {
+    let hitbox = game.add.graphics(level.hitboxes[i].x,level.hitboxes[i].y);
+    hitbox.drawRect(0,0,level.hitboxes[i].width,level.hitboxes[i].height);
+    game.physics.p2.enable(hitbox, false);
+    hitbox.body.static = true;
+    hitboxArr.push(hitbox);
+  }
+  //create humans based on level number
+  for(let i=0; i<levels[index].humans; i++){
+    humanArr[i] = new human('boy', positions[i].x, positions[i].y);
+    console.log("current level is: "+currentLevel);
+  }
+}
+
+function unloadLevel(index){
+  var level = levels[index];
+  level.sprite.destroy();
+  // ghost.destroy();
+  //TODO remove hitboxes related to this level
+  while(hitboxArr.length > 0){
+    let hitbox = hitboxArr.pop();
+    hitbox.body = null;
+    hitbox.destroy();
+  }
+}
